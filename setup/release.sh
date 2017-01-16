c326b9dce35cbd38e1992849ae30bfd100c8b75d
Nir Izraeli <nirizr@gmail.com>
2017-01-16 12:44:05 +0200

    Fix package rm path
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- setup/release.sh
+++ setup/release.sh
@@ -4,4 +4,4 @@ python ./setup/setup_idaplugin.py sdist --dist-dir ./setup/idaplugin_dist --form
 python ./setup/setup_server.py sdist --dist-dir ./setup/server_dist --formats=zip upload -r pypitest
 
 # cleanup
-rm -r ./setup/idaplugin_dist ./setup/server_dist ./rematch_idaplugin.egg-info ./rematch-server.egg-info
+rm -r ./setup/idaplugin_dist ./setup/server_dist ./rematch_idaplugin.egg-info ./rematch_server.egg-info

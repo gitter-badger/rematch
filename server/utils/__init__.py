a55ded2f63ec85064101749c48537101b3d71560
Nir Izraeli <nirizr@gmail.com>
2017-02-05 19:39:48 +0200

    Fix template rendering for lists
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- server/utils/__init__.py
+++ server/utils/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 
-class ViewSetTemplateMixin:
+class ViewSetTemplateMixin(object):
   def get_app_name(self):
     return self.__class__.__module__.lower().replace('.views', '')
 
@@ -13,3 +13,10 @@ class ViewSetTemplateMixin:
     model_template_name = os.path.join(self.get_model_name(), page)
     app_template_name = os.path.join(self.get_app_name(), page)
     return [model_template_name, app_template_name, page]
+
+  def list(self, request):
+    response = super(ViewSetTemplateMixin, self).list(request)
+
+    if request.accepted_renderer.format == 'html':
+      response.data = {'data': response.data}
+    return response
